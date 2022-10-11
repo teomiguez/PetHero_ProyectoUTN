96773fec03b310986491c5e74bf16f53c044533a
teomiguez <teo.miguez215@gmail.com>
2022-10-11 11:21:12 -0300

    create_controllers

--- Config/Config.php
+++ Config/Config.php
@@ -2,10 +2,9 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/UTN/TP6/ExerciseTwo/");
+define("FRONT_ROOT", "/UTN/4_CuartoCuatrimestre/PetHero_UTN2022/");
 define("VIEWS_PATH", "Views/");
-define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
-define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
 ?>
 
