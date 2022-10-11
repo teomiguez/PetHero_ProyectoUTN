934ad74c2ddfbde388b3132c6e221014215c891c
teomiguez <teo.miguez215@gmail.com>
2022-10-11 20:58:02 -0300

    complet_DueñoController_y_GuardianController

---
+++ Controllers/DueñoController.php
@@ -0,0 +1,46 @@
+<?php
+    namespace Controllers;
+
+    use DAO\DueñoDAO as DueñoDAO;
+    use Models\Dueño as Dueño;
+
+    class DueñoController 
+    {
+
+        private $dueñoDAO;
+
+        public function __contruct()
+        {
+            $this->dueñoDAO = new DueñoDAO();
+        } 
+
+        public function ShowAddView()
+        {
+            require_once(VIEWS_PATH."Register.php");
+        }
+
+        public function ShowListView()
+        {
+            $dueñoList = $this->dueñoDAO->getAll();
+            
+            require_once(VIEWS_PATH."GuardianHome.php");
+        }
+
+        public function Add($name, $last_name, $dni, $tel, $email, $pass)
+        {
+            $dueño = new Guardian();
+
+            $dueño->setId_dueño();
+            $dueño->setNombre($name);
+            $dueño->setApellido($last_name);
+            $dueño->setDni($dni);
+            $dueño->setTelefono($tel);
+            $dueño->setEmail($email);
+            $dueño->setContraseña($pass);
+
+            $this->dueñoDAO->Add($dueño);
+
+            require_once(VIEWS_PATH."Home.php");
+        }
+    }
+?>
---
+++ Controllers/GuardianController.php
@@ -0,0 +1,50 @@
+<?php
+    namespace Controllers;
+
+    use DAO\GuiardianDAO as GuardianDAO;
+    use Models\Guardian as Guardian;
+
+    class DueñoController 
+    {
+
+        private $guardianDAO;
+
+        public function __contruct()
+        {
+            $this->guardianDAO = new GuardianDAO();
+        } 
+
+        public function ShowAddView()
+        {
+            require_once(VIEWS_PATH."Register.php");
+        }
+
+        public function ShowListView()
+        {
+            $dueñoList = $this->dueñoDAO->getAll();
+            
+            require_once(VIEWS_PATH."DueñoHome.php");
+        }
+
+        public function Add($name, $last_name, $dni, $tel, $dir, $email, $pass, $reputacion, $tipoMascota, $precio)
+        {
+            $guardian = new Guardian();
+
+            $guardian->setId_guardian();
+            $guardian->setNombre($name);
+            $guardian->setApellido($last_name);
+            $guardian->setDni($dni);
+            $guardian->setTelefono($tel);
+            $guardian->setDireccion($dir);
+            $guardian->setEmail($email);
+            $guardian->setContraseña($pass);
+            $guardian->setReputacion($reputacion);
+            $guardian->setTamañoMascotaPref($tipoMascota);
+            $guardian->setPrecio($precio);
+
+            $this->guardianDAO->Add($guardian);
+
+            require_once(VIEWS_PATH."Home.php");
+        }
+    }
+?>
