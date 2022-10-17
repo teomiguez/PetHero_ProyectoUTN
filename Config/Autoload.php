fb3324994744258b9b644159fced11a87b4c91d6
teomiguez <teo.miguez215@gmail.com>
2022-10-17 15:40:43 -0300

    changeConfig

--- Config/Autoload.php
+++ Config/Autoload.php
@@ -1,15 +1,14 @@
 <?php namespace Config;
-
+	
     class Autoload {
-
+        
         public static function Start() {
-
-            spl_autoload_register(function($className) 
-            {
+            spl_autoload_register(function($className)
+			{
                 $classPath = ucwords(str_replace("\\", "/", ROOT.$className).".php");
-
-                include_once($classPath);
-            });
+                
+				include_once($classPath);
+			});
         }
     }
 ?>
