dbb9eb34fd03cd31e74ca343188b46903566093d
teomiguez <teo.miguez215@gmail.com>
2022-10-18 09:15:34 -0300

    change_Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -9,3 +9,5 @@ define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT . VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT . VIEWS_PATH . "js/");
 define("IMG_PATH", VIEWS_PATH . "images/");
+
+?>
